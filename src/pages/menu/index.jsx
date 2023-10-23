212b47aac02291bc18265792945e13e074b0583f
damassetiadi99 <damassetiadi999@gmail.com>
2023-10-24 02:54:37 +0700

    styling tipis-tipis

--- src/pages/menu/index.jsx
+++ src/pages/menu/index.jsx
@@ -1,237 +1,276 @@
 import axios from "axios";
-import { Fragment, useEffect, useState,useCallback } from "react";
-import { Link, Navigate, useNavigate } from "react-router-dom"
+import { Fragment, useEffect, useState, useCallback } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import Alert from "../../component/alert";
-import "./menu.css"
+import "./menu.css";
 import { Bounce, toast, ToastContainer, Zoom } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Navigation from "../../component/navbar";
-import { Container, Row, Col, Form, InputGroup, FormControl, Button } from 'react-bootstrap';
-import Footer from "../../component/footer"
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  InputGroup,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import Footer from "../../component/footer";
 
+export default function Menu() {
+  const [data, setData] = useState(null);
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertData, setAlertData] = useState({
+    type: "",
+    message: "",
+  });
+  const [searchQuery, setSearchQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(0);
 
+  const getData = useCallback(
+    (page) => {
+      const token = localStorage.getItem("token");
+      axios
+        .get(import.meta.env.VITE_BASE_URL + `/recipe`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          params: {
+            page: page,
+            limit: 5,
+            searchBY: "title",
+            search: searchQuery,
+          },
+        })
+        .then((res) => {
+          console.log(res);
+          // toast.success("Get Data Successfully", { toastId: "1" });
+          setData(res.data);
+          setTotalPage(res.data.pagination.totalPage);
+        })
+        .catch((err) => {
+          console.log(err);
+          // toast.error("Recipe Not Found", { toastId: "1" });
+        });
+    },
+    [searchQuery]
+  );
 
+  const handleSearchSubmit = async (event) => {
+    event.preventDefault();
+    setCurrentPage(1);
+    getData(1);
+  };
 
-export default function Menu(){
- const [data,setData] = useState(null)
- const [showAlert,setShowAlert]=useState (false)
- const [alertData,setAlertData] = useState({
-  type : "",
-  message : ""
- })
- const [searchQuery, setSearchQuery] = useState("");
- const [currentPage, setCurrentPage] = useState(1);
- const [totalPage, setTotalPage] = useState(0);
+  const handleNext = useCallback(() => {
+    if (currentPage < totalPage) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  }, [currentPage, totalPage]);
 
- const getData = useCallback(
-   (page) => {
-     const token = localStorage.getItem("token");
-     axios
-       .get(import.meta.env.VITE_BASE_URL + `/recipe`, {
-         headers: {
-           Authorization: `Bearer ${token}`,
-         },
-         params: {
-           page: page,
-           limit: 5,
-           searchBY: "title",
-           search: searchQuery,
-         },
-       })
-       .then((res) => {
-         console.log(res);
-         // toast.success("Get Data Successfully", { toastId: "1" });
-         setData(res.data);
-         setTotalPage(res.data.pagination.totalPage);
-       })
-       .catch((err) => {
-         console.log(err);
-         // toast.error("Recipe Not Found", { toastId: "1" });
-       });
-   },
-   [searchQuery]
- );
+  const handlePrevious = useCallback(() => {
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+  }, [currentPage]);
 
- const handleSearchSubmit = async (event) => {
-   event.preventDefault();
-   setCurrentPage(1);
-   getData(1);
- };
+  const handleSearchChange = useCallback((event) => {
+    setSearchQuery(event.target.value);
+  }, []);
 
- const handleNext = useCallback(() => {
-   if (currentPage < totalPage) {
-     setCurrentPage((prevPage) => prevPage + 1);
-   }
- }, [currentPage, totalPage]);
+  useEffect(() => {
+    getData(currentPage);
+  }, [getData, currentPage]);
 
- const handlePrevious = useCallback(() => {
-   if (currentPage > 1) {
-     setCurrentPage((prevPage) => prevPage - 1);
-   }
- }, [currentPage]);
+  const nextDisabled = currentPage >= totalPage;
+  const previousDisabled = currentPage === 1;
+  const deleteData = (id) => {
+    const token = localStorage.getItem("token");
+    axios
+      .delete(`https://rich-blue-scorpion-robe.cyclic.app/recipe/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        setData(res.data.data);
+        getData();
+        toast.success("berhasil hapus data");
+        setAlertData({
+          ...alertData,
+          type: "warning",
+          message: "berhasil hapus data",
+        });
+        setShowAlert(true);
+      })
+      .catch((err) => {
+        console.log(err);
+        getData();
+        toast.error("gagal menghapus data");
+        setAlertData({
+          ...alertData,
+          type: "warning",
+          message: "gagal hapus data",
+        });
+        setShowAlert(true);
+      });
+  };
 
- const handleSearchChange = useCallback((event) => {
-   setSearchQuery(event.target.value);
- }, []);
-
- useEffect(() => {
-   getData(currentPage);
- }, [getData, currentPage]);
-
- const nextDisabled = currentPage >= totalPage;
- const previousDisabled = currentPage === 1;
-  const deleteData = (id)=>{
-    const token = localStorage.getItem('token')
-    axios.delete(`https://rich-blue-scorpion-robe.cyclic.app/recipe/${id}`,{headers :{
-      Authorization : `Bearer ${token}`
-    }})
-    .then((res)=>{
-      
-
-      console.log(res)
-      setData(res.data.data)
-      getData()
-      toast.success('berhasil hapus data')
-      setAlertData({...alertData,type:"warning",message:"berhasil hapus data"})
-            setShowAlert(true)
-    
-    })
-    .catch((err)=>{
-      console.log(err)
-      getData()
-      toast.error('gagal menghapus data')
-      setAlertData({...alertData,type:"warning",message:"gagal hapus data"})
-      setShowAlert(true)
-    })
-  }
-
-    return(
-      
-      <Fragment>
-         <ToastContainer
-          position="top-center"
-          autoClose={2000}
-          hideProgressBar={false}
-          transition={Zoom}
-          newestOnTop
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="colored"
-        /> 
-        {/* html rb */}
-        <Navigation/>
-        <Container className="my-5">
-       
-      <Row>
-        <Col md={12}>
-          <h2 className="fw-bold color mt-5">
-            Discover Recipe <br />
-            & Delicious Food
-          </h2>
-        </Col>
-        <Col md={8} className="mt-3 mb-5">
-          <Form inline className="d-flex justify-content-center" onSubmit={handleSearchSubmit}>
-          
-            <InputGroup className="gap-4">
-              <FormControl
-                type="text"
-                className="form-control rounded"
-                placeholder=" Search"
-                aria-label="Search"
-                onChange={handleSearchChange}
-                value={searchQuery}
-              />
-              <div className="input-group-append">
-                <Button variant="warning" className="fw-bold text-white" type="submit">
-                  Search
-                </Button>
-              </div>
-            </InputGroup>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
-    <Container>
-      <Row>
-        <Col md={3} className="mb-2">
-          <Button className="fw-bold text-white btn-category">New</Button>
-        </Col>
-        <Col md={3} className="mb-2">
-          <Button className="fw-bold text-white btn-category">Popular</Button>
-        </Col>
-        <Col md={3} className="mb-2">
-          <Button className="fw-bold text-white btn-category2">Vegetarian</Button>
-        </Col>
-        <Col md={3} className="mb-2">
-          <Button className="fw-bold text-white btn-category2">Breakfast</Button>
-        </Col>
-      </Row>
-    </Container>
-    <Container className="my-5 shadow">
-      {data?.data?.map((item,index)=>{
-        return(
-          <div key={index}>
-          
-          <Row>
-        <Col md={4} className="d-flex justify-content-center align-items-center">
-          <Link to={`/detail-menu/${item.id}`}>
-          <img
-            src={item.photo}
-            width={300}
-            height={350}
-            alt="menu"
-            className="image-menu"
-          />
-          </Link>
-        </Col>
-        <Col md={8}>
-          <div>
-            <h5 className="fw-bold">
-              {item.title}
-            </h5>
-            <p className="fs-5 m-0">Ingredients</p>
-            <ul>
+  return (
+    <Fragment>
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        transition={Zoom}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+      {/* html rb */}
+      <Navigation />
+      <Container className="my-5">
+        <Row>
+          <Col md={12}>
+            <h2 className="fw-bold color mt-5">
+              Discover Recipe <br />& Delicious Food
+            </h2>
+          </Col>
+          <Col md={8} className="mt-3 mb-5">
+            <Form
+              inline
+              className="d-flex justify-content-center"
+              onSubmit={handleSearchSubmit}
+            >
+              <InputGroup className="gap-4">
+                <FormControl
+                  type="text"
+                  className="form-control rounded"
+                  placeholder=" Search"
+                  aria-label="Search"
+                  onChange={handleSearchChange}
+                  value={searchQuery}
+                />
+                <div className="input-group-append">
+                  <Button
+                    variant="warning"
+                    className="fw-bold text-white"
+                    type="submit"
+                    size="lg"
+                  >
+                    Search
+                  </Button>
+                </div>
+              </InputGroup>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+      <Container className="d-flex justify-content-space-between">
+        <Row>
+          <Col md={3} className="mb-2">
+            <Button className="fw-bold text-white" variant="warning">
+              New
+            </Button>
+          </Col>
+          <Col md={3} className="mb-2">
+            <Button className="fw-bold text-white" variant="warning">
+              Popular
+            </Button>
+          </Col>
+          <Col md={3} className="mb-2">
+            <Button className="fw-bold text-white " variant="success">
+              Vegetarian
+            </Button>
+          </Col>
+          <Col md={3} className="mb-2">
+            <Button className="fw-bold text-white " variant="success">
+              Breakfast
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+      <Container className="my-5 shadow">
+        {data?.data?.map((item, index) => {
+          return (
+            <div key={index}>
+              <Row>
+                <Col
+                  md={4}
+                  className="d-flex justify-content-center align-items-center my-4 gap-2"
+                >
+                  <Link to={`/detail-menu/${item.id}`}>
+                    <img
+                      src={item.photo}
+                      width={300}
+                      height={350}
+                      alt="menu"
+                      className="image-menu border-radius-3 shadow-2"
+                    />
+                  </Link>
+                </Col>
+                <Col md={8}>
+                  <div className="my-5">
+                    <h5 className="fw-bold">{item.title}</h5>
+                    <p className="fs-5 m-0">Ingredients</p>
+                    <ul>
                       {item.ingredients.split(",").map((ingredient, index) => (
                         <li key={index}>{ingredient.trim()}</li>
                       ))}
                     </ul>
-            <Button
-              className="mb-2 text-white rounded border border-0 p-1 fw-bold"
-              style={{ backgroundColor: '#efc81a' }}
-            >
-              10 Likes - 12 Comments - 3 Bookmarks
-            </Button>
-            <div className="d-flex gap-2 my-2">
-              {/* <Image
+                    <Button
+                      className="mb-2 text-white rounded border border-0 p-1 fw-bold"
+                      style={{ backgroundColor: "#efc81a" }}
+                    >
+                      10 Likes - 12 Comments - 3 Bookmarks
+                    </Button>
+                    <div className="d-flex gap-2 my-2">
+                      {/* <Image
                 className="rounded-circle"
                 src="../../assets/"
                 alt="Profile"
                 width="40px"
                 height="40px"
               /> */}
-              <p className="m-0 d-flex align-items-center fw-semibold">{item.author}</p>
-            </div>
-          </div>
-        </Col>
-      </Row>
-      <Link to={`/update-menu/${item.id}`}>
-                    <button className="btn btn-primary">update</button>
+                      <p className="m-0 d-flex align-items-center fw-semibold">
+                        {item.author}
+                      </p>
+                    </div>
+                    
+              <div className="md-6 d-flex justify-content-space-between gap-4 my-4 ml-3">
+                <Link to={`/update-menu/${item.id}`}>
+                  <button className="btn btn-primary" md={3}>
+                    Edit Menu
+                  </button>
                 </Link>
-            <button className="btn btn-danger" onClick={()=>deleteData(item.id)}>Delete</button>
-     
-          </div>
-        )
-      })}
-      
-     
-    </Container>
+                <button
+                  className="btn btn-danger"
+                  md={4}
+                  onClick={() => deleteData(item.id)}
+                >
+                 Delete Menu
+                </button>
+              </div>
+                  </div>
+                </Col>
+              </Row>
+
+            </div>
+          );
+        })}
+      </Container>
       <div className="items-center">
-        <Link to={'/inputmenu'}> Input Menu</Link>
-        {showAlert && <Alert type={alertData.type} message={alertData.message} />}
-      {/* <h1>Menu</h1>
+        <Link to={"/inputmenu"}> Input Menu</Link>
+        {showAlert && (
+          <Alert type={alertData.type} message={alertData.message} />
+        )}
+        {/* <h1>Menu</h1>
       {data?.map((item,index)=>{
         return(
           <div key={item.id} onClick={()=>console.log(item.id)}>
@@ -242,23 +281,32 @@ export default function Menu(){
                 </Link>
             <button className="btn btn-danger" onClick={()=>deleteData(item.id)}>Delete</button> */}
 
-      {/* </div> */}
+        {/* </div> */}
         {/* )
       })} */}
-      <div className="d-flex justify-content-center gap-5 mb-5">
-      <Button onClick={handlePrevious} variant="warning" className="fw-bold text-white" type="submit">
-                  previous
-                </Button>
-                <p className="text-dark">{data?.pagination?.pageNow}</p> From <p>{data?.pagination?.totalPage}</p>
-                <Button onClick={handleNext} variant="warning" className="fw-bold text-white" type="submit">
-                  next
-                </Button>
+        <div className="d-flex justify-content-center gap-5 mb-5">
+          <Button
+            onClick={handlePrevious}
+            variant="warning"
+            className="fw-bold text-white"
+            type="submit"
+          >
+            previous
+          </Button>
+          <p className="text-dark">{data?.pagination?.pageNow}</p> From{" "}
+          <p>{data?.pagination?.totalPage}</p>
+          <Button
+            onClick={handleNext}
+            variant="warning"
+            className="fw-bold text-white"
+            type="submit"
+          >
+            next
+          </Button>
         </div>
-
       </div>
-    
-      <Footer/>
-      </Fragment>
-      
-    )
-  }
+
+      <Footer />
+    </Fragment>
+  );
+}
